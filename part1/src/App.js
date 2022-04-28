4fc9a3751064287a3775f3d5b757559d7daf5c22
TheS1lentArr0w <paulocruz0123@hotmail.com>
2022-04-28 12:38:53 +0100

    Completed part1a notes

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,21 +1,22 @@
-const App = () => {
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="George" />
-      <Hello name="Daisy" />
-    </div>
-  )
-}
-
 const Hello = (props) => {
   return (
     <div>
-      <p>Hello {props.name}</p>
+      <p>Hello {props.name}, you are {props.age} years old</p>
     </div>
   )
 }
 
+const App = () => {
+  const name = 'Peter'
+  const age = 10
 
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26+10} />
+      <Hello name={name} age={age} />
+    </div>
+  )
+}
 
 export default App
